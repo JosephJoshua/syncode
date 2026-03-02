f5f2349e957ff3a5547c5e3318d40cabbf102953
JosephJoshua <jj.anggita@gmail.com>
2026-03-02 17:22:41 +0800

    chore(infra): configure git hooks

---
+++ commitlint.config.mjs
@@ -0,0 +1,43 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'web',
+        'api',
+        'realtime',
+        'executor',
+        'db',
+        'shared',
+        'ui',
+        'infra',
+        'ci',
+        'docs',
+        'deps',
+      ],
+    ],
+    'scope-empty': [2, 'never'],
+    'subject-min-length': [2, 'always', 10],
+    'subject-max-length': [2, 'always', 100],
+    'body-max-line-length': [0, 'always', Infinity],
+  },
+};
